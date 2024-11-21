f67b37958514abec795415ecf543f04a6212ceba
riya <riya@samyotech.com>
2024-11-21 12:41:00 +0530

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { CssBaseline, Box, StyledEngineProvider } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
+import { useLocation } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
+import { Grid } from 'react-loader-spinner';
 
-// routing
 import Routes from 'routes';
 
-// defaultTheme
 import themes from 'themes';
 
-// project imports
 import NavigationScroll from 'layout/NavigationScroll';
-
-// ==============================|| APP ||============================== //
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [location]);
 
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={themes(customization)}>
         <CssBaseline />
         <ToastContainer />
-        <NavigationScroll>
-          <Routes />
-        </NavigationScroll>
+        {loading ? (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '100vh'
+            }}
+          >
+            <Grid height="80" width="80" radius="9" color="#7951bf" ariaLabel="loading" visible={true} />
+          </Box>
+        ) : (
+          <NavigationScroll>
+            <Routes />
+          </NavigationScroll>
+        )}
       </ThemeProvider>
     </StyledEngineProvider>
   );
