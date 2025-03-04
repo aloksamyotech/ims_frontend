7d19502ba2bcb506c58a43328e79041a71b95f5e
riya <riya@samyotech.com>
2025-03-04 10:49:57 +0530

    show default logo

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -1,6 +1,7 @@
 import { useTheme } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 import { ButtonBase } from '@mui/material';
+import LogoInvento from 'assets/images/StockSmart.png';
 
 const user = localStorage.getItem('user');
 const userObj = user ? JSON.parse(user) : null;
@@ -9,12 +10,15 @@ const BASE_URL = 'https://ims.samyotech.in/api';
 
 const Logo = () => {
   const theme = useTheme();
-  const [logoUrl, setLogoUrl] = useState('/StockSmart.png');
+  const [logoUrl, setLogoUrl] = useState(LogoInvento);
+
+  useEffect(() => {
+    console.log("Logo URL updated:", logoUrl);
+  }, [logoUrl]);
 
   useEffect(() => {
     if (userObj?.logo) {
       let formattedPath = userObj.logo;
-
       if (!formattedPath.startsWith('/')) {
         formattedPath = `/${formattedPath}`;
       }
@@ -22,15 +26,15 @@ const Logo = () => {
       const fullUrl = formattedPath.startsWith('http') ? formattedPath : `${BASE_URL}${formattedPath}`;
       setLogoUrl(fullUrl);
     } else {
-      setLogoUrl('/StockSmart.png'); 
+      setLogoUrl(LogoInvento);
     }
-  }, []);
+  }, [userObj]); 
 
   return (
     <ButtonBase>
       <img
         alt="Company Logo"
-        src={logoUrl || '/StockSmart.png'}
+        src={logoUrl}
         style={{ height: '70px', width: '180px', objectFit: 'contain' }}
       />
     </ButtonBase>
